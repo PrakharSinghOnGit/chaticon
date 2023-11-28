3f2c04fad56a4fe1ac3600085c043ea06b61589d
PrakharSinghOnGit <singhprakhar1209@gmail.com>
2023-11-28 21:40:44 +0530

    Refactor UserCard component and update styles

--- app/chaticon/UserCard.tsx
+++ app/chaticon/UserCard.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 
-const UserCard = () => {
+interface UserCardProps {
+  img: string;
+  name: string;
+  msg: string;
+  time: string;
+}
+
+const UserCard: React.FC<UserCardProps> = ({ img, name, msg, time }) => {
   return (
-    <div className="bg-backgroundColor rounded-xl flex h-max p-2 items-center gap-2 flex-shrink-0 self-stretch">
-      Helo User
+    <div className="bg-backgroundColor rounded-xl flex h-max p-2 items-center gap-2 flex-shrink-0 self-stretch relative select-none">
+      <img
+        className="rounded-lg pointer-events-none"
+        width={35}
+        src={img}
+        alt={name}
+      />
+      <div className="flex flex-col">
+        <span className="font-semibold text-sm">{name}</span>
+        <p className="text-xs">{msg}</p>
+        <p className="text-xs text-gray-500 absolute right-0 top-0 m-1 mr-2">
+          {time}
+        </p>
+      </div>
     </div>
   );
 };
--- app/chaticon/page.tsx
+++ app/chaticon/page.tsx
@@ -4,17 +4,20 @@ import UserCard from "./UserCard";
 
 const Page = () => {
   return (
-    <main className="bg-backgroundColor flex min-h-screen p-3 gap-3 items-center">
-      <div className="bg-sectionColor w-3/12 min-w-min flex p-3 flex-col items-center gap-3 flex-shrink-0 self-stretch rounded-xl">
+    <main className="bg-backgroundColor flex min-h-screen py-3 gap-3 items-center">
+      <div className="bg-sectionColor w-3/12 min-w-min flex p-3 flex-col items-center gap-3 flex-shrink-0 self-stretch rounded-r-xl">
         <div className="bg-backgroundColor w-full min-w-min flex p-3 flex-col items-center gap-3 self-stretch rounded-xl">
           <input type="text" placeholder="Serach Here ..." />
         </div>
         <Seperator />
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
-        <UserCard />
+        <UserCard
+          img={
+            "https://lh3.googleusercontent.com/a/ACg8ocJy2t4C7jxhf1Q1-vB9_ykVZXPIwNjZiOeHR3kH7jthWJvr=s576-c-no"
+          }
+          name={"Shaan Singh"}
+          msg={"Hello"}
+          time={"2 Min Ago"}
+        />
       </div>
       <div className="bg-sectionColor min-w-min flex p-3 flex-col items-center gap-3 flex-grow self-stretch rounded-xl"></div>
     </main>
